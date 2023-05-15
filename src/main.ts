37846cd82254d3896c8a114460791461e7957133
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-05-15 15:37:58 +0100

    new: setup swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('youdotrl')
+    .setDescription('Shorten your URLS')
+    .setVersion('0.0.1')
+    .build();
+
+  const swagDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, swagDocument);
 
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get('APP_PORT');
