dfa858619df7564d1942d7d9459294954666b6df
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-05-17 13:15:44 +0100

    chore: add API description

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -9,6 +9,9 @@ import { ApiTags } from '@nestjs/swagger';
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
+  /**
+   * Post a URL to be shortened
+   */
   @Post()
   async create(@Body() createUrlDto: CreateShortUrlDto): Promise<Url> {
     return this.urlService.create(createUrlDto);
